032e23ea0cd46617daac8c24d16940e3cb4ae658
Julien <papao1@hotmail.fr>
2017-07-30 15:22:08 +0200

    début du code js

---
+++ javascript/app.js
@@ -0,0 +1,42 @@
+var player1 = prompt("Votre nom");
+var boss;
+var health = 20;
+var damage = math.floor(math.random(6));
+
+
+function runFigth (player1, boss){
+
+
+
+
+}
+
+
+// function addition (str1, str2) {
+
+// var result;
+
+// result = "le resultat de ton addition est de " + (str1 + str2);
+
+// return alert(result);
+
+// }
+
+// var chiffre1 = Number (prompt("Coucou donne moi ton premier chiffre a additionner"));
+// var chiffre2 = Number (prompt("donne moi ton deuxieme chiffre a additionner"));
+// var sommeAdd2 = addition ( chiffre1, chiffre2);
+
+
+// function multiplication (str3, str4) {
+
+// var result;
+
+// result = "Pouf le resultat de ta multiplication est de " + str3 * str4 + " essaye de faire mieux";
+
+// return alert(result);
+
+// }
+
+// var chiffre3 = Number (prompt("Je sais en faire d'autre, donne moi ton premier chiffre a multiplier"));
+// var chiffre4 = Number (prompt("et maintenant ton multiplieur"));
+// var Multi3 = multiplication ( chiffre3, chiffre4);
